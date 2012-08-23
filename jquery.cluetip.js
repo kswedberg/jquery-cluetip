721e770e86104d83a76ed8a746e24fb8ba22bba2
Abhishek <akumar@faichi.com>
2012-08-23 12:18:53 +0530

    Fixed formating issue.

--- jquery.cluetip.js
+++ jquery.cluetip.js
@@ -804,22 +804,24 @@
 
 //Added jQuery ismouseover method to track mouse hover
 (function($){ 
-    $.mlp = {x:0,y:0}; // Mouse Last Position
-    function documentHandler(){
-        var $current = this === document ? $(this) : $(this).contents();
-        $current.mousemove(function(e){jQuery.mlp = {x:e.pageX,y:e.pageY}});
-        $current.find("iframe").load(documentHandler);
-    }
-    $(documentHandler);
-    $.fn.ismouseover = function(overThis) {  
-        var result = false;
-        this.eq(0).each(function() {  
-                var $current = $(this).is("iframe") ? $(this).contents().find("body") : $(this);
-                var offset = $current.offset();             
-                result =    offset.left<=$.mlp.x && offset.left + $current.outerWidth() > $.mlp.x &&
-                            offset.top<=$.mlp.y && offset.top + $current.outerHeight() > $.mlp.y;
-        });  
-        return result;
-    };  
+  $.mlp = {x:0,y:0}; // Mouse Last Position
+  function documentHandler(){
+    var $current = this === document ? $(this) : $(this).contents();
+    $current.mousemove(function(e){
+      jQuery.mlp = {x:e.pageX,y:e.pageY}
+    });
+    $current.find("iframe").load(documentHandler);
+  }
+  $(documentHandler);
+  $.fn.ismouseover = function(overThis) {  
+    var result = false;
+    this.eq(0).each(function() {  
+      var $current = $(this).is("iframe") ? $(this).contents().find("body") : $(this);
+      var offset = $current.offset();             
+      result = offset.left<=$.mlp.x && offset.left + $current.outerWidth() > $.mlp.x &&
+      offset.top<=$.mlp.y && offset.top + $current.outerHeight() > $.mlp.y;
+    });  
+    return result;
+  };  
 })(jQuery);
 
